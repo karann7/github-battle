3df02381f3dd6af8fab366d2a5c747bf2879afa1
Karan Singh <kar.n@icloud.com>
2017-06-25 14:06:12 -0700

    added boilerplate for the project, and set up webpack to use aliases

---
+++ app/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+	render() {
+		return (
+			<div>
+					Basic hello world react app.
+			</div>
+		);
+	}
+}
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+  entry: './app/index.js',
+  output: {
+			path: path.resolve(__dirname, 'dist'),
+			filename: 'index-bundle.js'
+  },
+	module: {
+		rules: [
+			{ test: /\.(js)$/, use: 'babel-loader' },
+			{ test: /\.css$/, use: [ 'style-loader', 'css-loader' ]}
+		]
+  },
+	resolve: {
+			alias: {
+				App: './components/App'
+    },
+    extensions: ['*', '.js', '.jsx']
+  },
+    plugins: [new HtmlWebpackPlugin({
+        template: 'app/index.html'
+    })]
+}
