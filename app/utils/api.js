3c1700977e2528ee1202986308a97055a3d3854e
Karan Singh <kar.n@icloud.com>
2017-06-30 14:37:47 -0700

    added multiple function to support the async request from github using axios

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,6 +1,57 @@
 import axios from 'axios';
 
+function getProfile(username) {
+  return axios.get(`https://api.github.com/users/${username}`)
+    .then((user)=>{
+      return user.data;
+    });
+}
+
+function getRepos (username) {
+  return axios.get(`https://api.github.com/users/${username}&per_page=100`);
+}
+
+function getStarCount (repos) {
+  return repos.data.reduce((count, repo)=>{
+    return count + repo.stargazers_count;
+  }, 0);
+}
+
+function calculateScore(profile, repos) {
+  let followers = profile.followers;
+  let totalStars = getStarCount(repos);
+  return (followers * 3) + totalStars;
+}
+
+function handleError(error) {
+  console.warn(error);
+  return null;
+}
+
+function getUserData(player) {
+  return axios.all([
+    getProfile(player),
+    getRepos(player)
+  ]).then((data)=>{
+    let profile = data[0];
+    let repos = data[1];
+    return {
+      profile,
+      score: calculateScore(profile, repos)
+    }
+  });
+}
+
+function sortPlayers (players) {
+  return players.sort((a, b)=> b.score - a.score);
+}
+
 export default {
+  battle: function(players) {
+    return axios.all(player.map(getUserData))
+      .then(sortPlayers)
+      .catch(handleError);
+  },
   fetchPopularRepos: function(language){
     var encodedURI = window.encodeURI(`https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`);
     return axios.get(encodedURI)
